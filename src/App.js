3c9d23de5abbe3ffc6ba010e270af04527fc54b1
Ilona Zaika <zaika.ilona.v@gmail.com>
2023-11-05 17:35:57 +0000

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './App.css';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          });
+      });
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App" >
+        <h1 className='app-title'>It's Halloween night</h1>
+        <SearchBox
+          className='search-box-monsters'
+          placeholder='Search guests'
+          onChangeHandler={onSearchChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
